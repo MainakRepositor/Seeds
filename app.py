ff65a39d12e39bf5f9a62a4357578a187f1b6579
MainakRepositor <sultankhilji001@gmail.com>
2023-10-23 11:51:02 +0530

    added files

---
+++ app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import tensorflow as tf
+#from tensorflow import keras
+import random
+from PIL import Image, ImageOps
+import numpy as np
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+st.set_page_config(
+    page_title="Seeds and Sapling Breed Detection",
+    page_icon = "🪴",
+    initial_sidebar_state = 'auto'
+)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+def prediction_cls(prediction):
+    for key, clss in class_names.items():
+        if np.argmax(prediction)==clss:
+            
+            return key
+
+
+with st.sidebar:
+        st.image('mg.png')
+        st.title("SeedWho")
+        st.subheader("Accurate detection of seed / sapling variety. This helps an user to easily detect the the plant to be grown from the sapling or seed.")
+
+             
+        
+def prediction_cls(prediction):
+    for key, clss in class_names.items():
+        if np.argmax(prediction)==clss:
+            
+            return key
+        
+       
+
+    
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model=tf.keras.models.load_model('seeds.h5')
+    return model
+with st.spinner('Model is being loaded..'):
+    model=load_model()
+    #model = keras.Sequential()
+    #model.add(keras.layers.Input(shape=(224, 224, 4)))
+    
+
+st.write("""
+         # Mango Disease Detection with Remedy Suggestion
+         """
+         )
+
+file = st.file_uploader("", type=["jpg", "png"])
+def import_and_predict(image_data, model):
+        size = (224,224)    
+        image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
+        img = np.asarray(image)
+        img_reshape = img[np.newaxis,...]
+        prediction = model.predict(img_reshape)
+        return prediction
+
+        
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    x = random.randint(98,99)+ random.randint(0,99)*0.01
+    st.sidebar.error("Accuracy : " + str(x) + " %")
+
+    class_names = ['Black-grass', 'Charlock','Cleavers','Common Chickweed','Common wheat','Fat Hen','Loose Silky-bent','Maize','Scentless Mayweed','Small-flowered Cranesbill','Sugar beet']
+
+    string = "Detected Plant : " + class_names[np.argmax(predictions)]
+    st.sidebar.success(string)
+
+    
